1b6cdc1f0bce7321c64ca126e2e665a2294a889c
Elwazer20 <ahmed.elwazergith@gmail.com>
2025-02-22 07:34:19 -0800

    create project note item

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
-import 'package:note_keeper/notes_view.dart';
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:note_keeper/notes_view.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const NoteKeeper());
+  runApp(
+    DevicePreview(
+      enabled: true, // Set to false in production
+      builder: (context) => const NoteKeeper(),
+    ),
+  );
 }
+
 class NoteKeeper extends StatelessWidget {
   const NoteKeeper({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme:  ThemeData(
+      debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context), // Uses the selected locale
+      builder: DevicePreview.appBuilder, // Wraps the app for preview
+      theme: ThemeData(
         brightness: Brightness.dark,
+        textTheme: GoogleFonts.poppinsTextTheme(), // Apply Poppins
       ),
       home: NotesView(),
     );
--- lib/views/note_item.dart
+++ lib/views/note_item.dart
@@ -1,26 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        ListTile(
-          title: Text('Flutter Tips', style: TextStyle(color: Colors.black)),
-          subtitle: Text(
-            'build yours career with Tharwat Samy',
-            style: TextStyle(color: Colors.black),
+    return Container(
+      padding: const EdgeInsets.only(top: 24, left: 16, right: 16, bottom: 24),
+      decoration: BoxDecoration(
+        color: Color(0xffffcc80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Text(
+                'build yours career with Tharwat Samy',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.4),
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(FontAwesomeIcons.trash, size: 24, color: Colors.black),
+            ),
           ),
-          trailing: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.delete, color: Colors.black),
+          Padding(
+            padding: const EdgeInsets.only(right: 14),
+            child: Text(
+              'Fab21 , 2025',
+              style: TextStyle(color: Colors.black.withOpacity(0.4)),
+            ),
           ),
-        ),
-        Text('Fab21 , 2025',style: TextStyle(color: Colors.black))
-      ],
+        ],
+      ),
     );
   }
 }
